195faf630a02b99e0a3a85eed5a72e3f5a1cb2a9
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-09-02 19:52:31 +0800

    Update dataVisual.py
    在散点图中增加主机厂

--- code/dataVisual.py
+++ code/dataVisual.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import data
+import solver
 import numpy as np
 
 
@@ -8,7 +9,7 @@ class DataVisual:
     def __init__(self):
         pass
 
-    def draw_scatter(self, x, y):
+    def draw_scatter(self, x, y, main_x, main_y):
 
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
@@ -16,6 +17,7 @@ class DataVisual:
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.scatter(x, y, marker='.')
+        ax.scatter(main_x, main_y, marker='x', s=50)
         plt.xlim(xmax=np.max(x)+0.1, xmin=np.min(x)-0.1)
         plt.ylim(ymax=np.max(y)+0.1, ymin=np.min(y)-0.1)
         plt.show()
@@ -24,5 +26,6 @@ class DataVisual:
 # test code
 # worker = DataVisual()
 # my_data = data.Data()
+# sol = solver.Solver()
 # x, y, _, _ = my_data.get_info()
-# worker.draw_scatter(x, y)
+# worker.draw_scatter(x, y, sol.machine_x, sol.machine_y)
